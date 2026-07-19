b32e9d9e0a17985d50edec8a0be77c8ac9317946
nancy <khandelwalnancy27@gmail.com>
2026-07-20 00:03:30 +0530

    Day 30 - Palindrome Partitioning II

---
+++ Day30/PalindromePartitioningII.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def minCut(self, s):
+
+        n = len(s)
+
+        pal = [[False] * n for _ in range(n)]
+
+        dp = [0] * n
+
+        for i in range(n):
+            dp[i] = i
+
+            for j in range(i + 1):
+
+                if s[j] == s[i] and (i - j < 2 or pal[j + 1][i - 1]):
+
+                    pal[j][i] = True
+
+                    if j == 0:
+                        dp[i] = 0
+                    else:
+                        dp[i] = min(dp[i], dp[j - 1] + 1)
+
+        return dp[-1]
+    
