05dc47259977c300a81d838d30fa52696ca378b5
nancy <khandelwalnancy27@gmail.com>
2026-07-20 00:01:14 +0530

    Day 30 - N-Queens

---
+++ Day30/NQueens.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def solveNQueens(self, n):
+
+        ans = []
+
+        col = set()
+        diag1 = set()
+        diag2 = set()
+
+        board = [["."] * n for _ in range(n)]
+
+        def backtrack(r):
+
+            if r == n:
+                ans.append(["".join(row) for row in board])
+                return
+
+            for c in range(n):
+
+                if c in col or r - c in diag1 or r + c in diag2:
+                    continue
+
+                col.add(c)
+                diag1.add(r - c)
+                diag2.add(r + c)
+
+                board[r][c] = "Q"
+
+                backtrack(r + 1)
+
+                board[r][c] = "."
+
+                col.remove(c)
+                diag1.remove(r - c)
+                diag2.remove(r + c)
+
+        backtrack(0)
+
+        return ans
+    
