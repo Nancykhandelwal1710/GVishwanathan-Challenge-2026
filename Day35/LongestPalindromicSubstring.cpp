a177c17c7d1dafc8e308ae48489dad164bfcece5
nancy <khandelwalnancy27@gmail.com>
2026-07-24 22:09:48 +0530

    Day 35 - Longest Palindromic Substring

---
+++ Day35/LongestPalindromicSubstring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size(), st = 0, len = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            int l = i, r = i;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                if (r - l + 1 > len) {
+                    len = r - l + 1;
+                    st = l;
+                }
+                l--;
+                r++;
+            }
+
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                if (r - l + 1 > len) {
+                    len = r - l + 1;
+                    st = l;
+                }
+                l--;
+                r++;
+            }
+        }
+
+        return s.substr(st, len);
+    }
+};
