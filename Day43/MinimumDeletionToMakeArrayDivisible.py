f7eee0b51c8914a7fb0e6f93b83ce371e36a839f
nancy <khandelwalnancy27@gmail.com>
2026-08-01 09:57:35 +0530

    Day 43 - Minimum Deletions to Make Array Divisible

---
+++ Day43/MinimumDeletionToMakeArrayDivisible.py
@@ -0,0 +1,22 @@
+class Solution(object):
+
+    def gcd(self, a, b):
+        while b:
+            a, b = b, a % b
+        return a
+
+    def minOperations(self, nums, numsDivide):
+
+        g = numsDivide[0]
+
+        for x in numsDivide[1:]:
+            g = self.gcd(g, x)
+
+        nums.sort()
+
+        for i in range(len(nums)):
+            if g % nums[i] == 0:
+                return i
+
+        return -1
+    
